61fdf3fa4a1dc868ab8f086882a51327de530aa4
Ahmad Azeez <azeeztriplea10@gmail.com>
2026-09-06 23:48:46 -0400

    Publish static preview build

---
+++ _next/static/chunks/3lf22mwz23oo_.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,98168,i=>{"use strict";var t=i.i(43476),o=i.i(32181),n=i.i(42295);i.s(["default",0,function({children:i}){return(0,t.jsx)(o.motion.div,{initial:{opacity:0,y:10},animate:{opacity:1,y:0},transition:{duration:.5,ease:n.EASE.out},children:i})}])}]);
